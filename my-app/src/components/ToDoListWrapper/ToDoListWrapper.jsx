66f8fa2a97030b63a1e1eedc58bbf5de52c34209
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-02-19 21:50:01 +0300

    updated todolist backGround

--- my-app/src/components/ToDoListWrapper/ToDoListWrapper.jsx
+++ my-app/src/components/ToDoListWrapper/ToDoListWrapper.jsx
@@ -1,8 +1,10 @@
 import classes from "./ToDoListWrapper.module.css";
+import logo from './back.png';
 
 const ToDoListWrapper = () => {
     return (
         <div>
+            <div><img src={logo} className="App-logo" alt="logo" /></div>
             <div className={classes.wrapper}></div>
         </div>
     )
