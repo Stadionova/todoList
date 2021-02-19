5c8acef1b01a4e3724ec11c71a51ba96b592607a
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-02-19 20:59:11 +0300

    created wrapper for toDoList

---
+++ my-app/src/components/ToDoListWrapper/ToDoListWrapper.jsx
@@ -0,0 +1,11 @@
+import classes from "./ToDoListWrapper.module.css";
+
+const ToDoListWrapper = () => {
+    return (
+        <div>
+            <div className={classes.wrapper}></div>
+        </div>
+    )
+}
+
+export default ToDoListWrapper;
