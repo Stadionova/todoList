a6a71c7b81b212426c3f09dadc3edd477a339800
Yulia Tribunskaya <ytribunskaya@kameleoon.com>
2021-05-31 17:21:31 +0300

    cleaned initialState.tasks default

--- src/store.js
+++ src/store.js
@@ -4,22 +4,7 @@ const initialState = {
     newTaskInputValue: '',
     newTaskCreated: 'false',
     maxId: 3,
-    tasks: [
-        {
-            1: {
-                id: 1,
-                value: 'hello'
-            },
-            2: {
-                id: 2,
-                value: 'hi'
-            },
-            3: {
-                id: 3,
-                value: 'yulia'
-            }
-        }
-    ]
+    tasks: [{}]
 };
 
 function toDoListReducer(state = initialState, action) { // reducer function, which get action & return changed state
